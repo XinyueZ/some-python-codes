e8d16b37e6281dca635388c486f52544cfc68814
XinyueZ <xinyue.zhao@immonet.de>
2018-03-20 12:41:47 +0100

    Updated most_prolific.py for another solution

--- most_prolific.py
+++ most_prolific.py
@@ -53,3 +53,33 @@ print(most_prolific(Beatles_Discography))
 Beatles_Discography = {'The Game': 1980, 'A Night at the Opera': 1975, 'Jazz': 1978, 'Queen II': 1974, 'A Day at the Races': 1976, 'News of the World': 1977, 'Queen': 1973, 'Sheer Heart Attack': 1974}
 print(most_prolific(Beatles_Discography))
 
+"""
+def most_prolific(discs): 
+    #We will store a dictionary of years 
+    #and number of albums per year     
+        years = {} 
+            maxyears = [] 
+                maxnumber = 0 
+                    for disc in discs: 
+                            year = discs[disc]
+                                    if year in years: 
+                                                years[year] += 1 
+                                                        else: 
+                                                                    years[year] = 1 
+
+                                                                    #find the year in which the maximum 
+                                                                    #number of albums was published 
+                                                                    #there are more elegant ways of accomplishing this, 
+                                                                    #but the code below works 
+                                                                        for year in years:
+                                                                                if years[year] > maxnumber: 
+                                                                                            maxyears=[] 
+                                                                                                        maxyears.append(year) 
+                                                                                                                    maxnumber = years[year] 
+                                                                                                                            elif years[year] == maxnumber and not (year in maxyears): 
+                                                                                                                                        maxyears.append(year) 
+                                                                                                                                            if (len(maxyears) == 1): 
+                                                                                                                                                    return maxyears[0] 
+                                                                                                                                                        else: 
+                                                                                                                                                                return maxyears
+"""
