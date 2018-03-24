2b1dde2f1074f6275fd94e61c88171d7336c9d5b
XinyueZ <xinyue.zhao@immonet.de>
2018-03-24 19:57:20 +0100

    Updated draw_shapes.py with functions

--- draw_shapes.py
+++ draw_shapes.py
@@ -1,25 +1,25 @@
 import turtle
 
-def draw_shapes():
-    window = turtle.Screen()
-    window.bgcolor("red")
+def draw_square():
     paint = turtle.Turtle()
-    paint.forward(100)
-    paint.right(90)
-    
-    paint.forward(100)
-    paint.right(90)
+    for _ in range(4):
+        paint.forward(100)
+        paint.right(90)
 
-    paint.forward(100)
-    paint.right(90)
-
-    paint.forward(100)
-    paint.right(90)
-
+def draw_circle():
     drawer = turtle.Turtle()
     drawer.shape("arrow")
     drawer.color("yellow")
     drawer.circle(40)
+
+
+
+def draw_shapes():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    draw_square()
+    draw_circle()
 
     window.exitonclick()
 
