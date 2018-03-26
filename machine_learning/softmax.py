bc194116935dab26f51b9e288875545d1f262979
XinyueZ <xinyue.zhao@immonet.de>
2018-03-26 23:30:36 +0200

    Added machine_learning/softmax.py

---
+++ machine_learning/softmax.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import exp
+from numpy import sum as summary
+from numpy import arange
+from numpy import vstack
+from numpy import ones_like
+from numpy import max as maxium
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    exp_x = exp(x - maxium(x))
+    return exp_x / summary(exp_x, axis = 0)
+
+scores = [3.0, 1.0, 0.2]
+print(softmax(scores))
+x = arange(-2.0, 6.0, 0.1)
+scores = vstack([x, ones_like(x), 0.2 * ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
