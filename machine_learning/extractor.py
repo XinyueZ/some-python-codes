ebd3be718da77ed8ce410a458de8aa81cb7636f5
XinyueZ <xinyue.zhao@immonet.de>
2018-04-05 15:55:52 +0200

    Added extractor.py

---
+++ machine_learning/extractor.py
@@ -0,0 +1,24 @@
+#
+# Util class provide extracting compressed files.
+#
+from os.path import splitext as split_text
+from os.path import isdir as is_dir
+from os.path import join
+
+class Extractor:
+    def __init__(self, src_object_fullname_list, saved_data_root = "."):
+        """
+        Construct the Extractor with collection of objects with fullnames.
+        The root directory where all objects would extracted into.
+        """
+        pass
+
+
+    def extract(self):
+        """
+        Extract all objects.
+        """
+        pass
+
+
+
